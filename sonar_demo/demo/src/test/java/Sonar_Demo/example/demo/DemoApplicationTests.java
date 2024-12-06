1d4317b3d4e9c0eb8899371ff65942076272c9fb
Subham Nandi <subhamnandiagt@gmail.com>
2024-12-06 16:08:44 +0530

    Add files via upload

---
+++ sonar_demo/demo/src/test/java/Sonar_Demo/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package Sonar_Demo.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
